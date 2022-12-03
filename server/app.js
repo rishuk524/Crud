c0d33d7930c0e260b1c8873da5a1c4ef32248c41
Rishu kumar <rishuk524@gmail.com>
2022-12-03 21:44:46 +0530

    fixed bug

--- server/app.js
+++ server/app.js
@@ -7,6 +7,13 @@ const app = express()
 //Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 connectodb()
 
